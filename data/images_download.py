9dae44561c7d1749e597c9f90512bccfc1e6460d
Jimena Santonja <jimenasantonjagonzl@gmail.com>
2025-04-19 11:55:56 +0200

    Create images_download.py

---
+++ data/images_download.py
@@ -0,0 +1,44 @@
+# Instalación de la libreríia duckduckgo-search
+pip install duckduckgo-search pillow
+pip install duckduckgo-search --upgrade
+
+# Carga de librerías
+import os
+import pandas as pd
+import requests
+from PIL import Image
+from io import BytesIO
+from duckduckgo_search import DDGS
+
+# Cargar el dataset original
+dataset = pd.read_csv("structured_dataset.csv")
+
+# Carpeta donde se guardarán las imágenes
+output_dir = "dowloaded_images"
+os.makedirs(output_dir, exist_ok=True)
+
+with DDGS() as ddgs:
+    for index, row in dataset.iterrows():
+        query = f"{row['picture'].replace('-', ' ').replace('.jpg', '')} {row['artist']} {row['style']}"
+        print(f"[{index+1}] Buscando: {query}")
+
+        try:
+            results = ddgs.images(keywords=query, max_results=1)
+            result_list = list(results)
+            if result_list:
+                url = result_list[0]["image"]
+                response = requests.get(url, timeout=10)
+                img = Image.open(BytesIO(response.content)).convert("RGB")
+                
+                # Guardar la imagen en la carpeta 
+                filename = os.path.join(output_dir, row['picture'])
+                img.save(filename)
+
+                print(f" Guardada: {filename}")
+            else:
+                print(f" No se encontraron resultados para: {query}")
+        except Exception as e:
+            print(f" Error al procesar {query}: {e}")
+
+
+
